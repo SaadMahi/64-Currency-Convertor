2e18da7eeb6175a7c13f93311d68af6419a022a1
Mahimkar Saad Tahzeeb <117567622+SaadMahi@users.noreply.github.com>
2023-10-17 17:17:39 +0530

    🎉 feat: project finalization

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,76 @@
+import { useEffect, useState } from 'react';
 import './App.css';
 
+import Loader from './components/Loader';
+
 function App() {
+  // * userInput amount extractor
+  const [userInput, setUserInput] = useState(1);
+  // //console.log(userInput);
+
+  // * user's converted value
+  const [convertedValue, setConvertedValue] = useState();
+
+  // * from currency
+  const [fromCurrency, setFromCurrency] = useState('INR');
+  // //console.log(fromCurrency);
+
+  // * to currency
+  const [toCurrency, setToCurrency] = useState('USD');
+  // //console.log(toCurrency);
+
+  // * loader on loading currency
+  const [loading, setLoading] = useState(false);
+
+  // ! FECTCH CURRENCY DATA
+  useEffect(() => {
+    async function currencyConvertor() {
+      setLoading(true);
+      const res = await fetch(
+        `https://api.frankfurter.app/latest?amount=${userInput}&from=${fromCurrency}&to=${toCurrency}`
+      );
+
+      const data = await res.json();
+      // //console.log(data.rates[toCurrency]);
+      setConvertedValue(data.rates[toCurrency]);
+      setLoading(false);
+    }
+
+    if (fromCurrency === toCurrency) return setConvertedValue(userInput);
+
+    currencyConvertor();
+  }, [userInput, fromCurrency, toCurrency]);
+
   return (
     <div>
-      <input type='text' />
-      <select>
-        <option value='USD'>USD</option>
-        <option value='EUR'>EUR</option>
-        <option value='CAD'>CAD</option>
-        <option value='INR'>INR</option>
+      <input
+        onChange={(e) => setUserInput(Number(e.target.value))}
+        type='number'
+        value={!userInput ? '' : userInput}
+      />
+
+      <select
+        value={fromCurrency}
+        onChange={(e) => setFromCurrency(e.target.value)}
+        disabled={loading}
+      >
+        <option>USD</option>
+        <option>EUR</option>
+        <option>CAD</option>
+        <option>INR</option>
       </select>
-      <select>
-        <option value='USD'>USD</option>
-        <option value='EUR'>EUR</option>
-        <option value='CAD'>CAD</option>
-        <option value='INR'>INR</option>
+
+      <select
+        value={toCurrency}
+        onChange={(e) => setToCurrency(e.target.value)}
+        disabled={loading}
+      >
+        <option>USD</option>
+        <option>EUR</option>
+        <option>CAD</option>
+        <option>INR</option>
       </select>
-      <p>OUTPUT</p>
+      <p>converted value: {loading ? <Loader /> : convertedValue}</p>
     </div>
   );
 }
